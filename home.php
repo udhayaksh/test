e1f59ac3ead165a73e1495bc35af87864bcde402
udhayaksh <33419066+udhayaksh@users.noreply.github.com>
2017-11-06 17:11:48 +0530

    test files upload

---
+++ home.php
@@ -0,0 +1,152 @@
+	<html>
+	
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-3 cradHeight">
+				<div class="offCard">s
+					<div class="top">
+						<p>Shop Name</p>
+						<div class="offerPrice"><p class="offLabel">10% off</p></div>
+					</div>
+					<div class="shopLogo">
+						<img src="zipker-coupon-codes.jpg" />
+					</div>
+					<div class="offerContent">
+						<p>Minimum 30% – 80% Off On Best Selling Products + Free Shipping On All Orders</p>
+
+					</div>
+					<div class="getOffBtn">
+					<button class="getOff"><a href="">Get</a><i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 cradHeight">
+				<div class="offCard">
+					<div class="top">
+						<p>Shop Name</p>
+						<div class="offerPrice"><p class="offLabel">10% off</p></div>
+					</div>
+					<div class="shopLogo">
+						<img src="zipker-coupon-codes.jpg" />
+					</div>
+					<div class="offerContent">
+						<p>Minimum 30% – 80% Off On Best Selling Products + Free Shipping On All Orders</p>
+
+					</div>
+					<div class="getOffBtn">
+					<button class="getOff"><a href="list.php">Get</a><i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 cradHeight">
+				<div class="offCard">
+					<div class="top">
+						<p>Shop Name</p>
+						<div class="offerPrice"><p class="offLabel">10% off</p></div>
+					</div>
+					<div class="shopLogo">
+						<img src="zipker-coupon-codes.jpg" />
+					</div>
+					<div class="offerContent">
+						<p>Minimum 30% – 80% Off On Best Selling Products + Free Shipping On All Orders</p>
+
+					</div>
+					<div class="getOffBtn">
+					<button class="getOff"><a href="list.php">Get</a><i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 cradHeight">
+				<div class="offCard">
+					<div class="top">
+						<p>Shop Name</p>
+						<div class="offerPrice"><p class="offLabel">10% off</p></div>
+					</div>
+					<div class="shopLogo">
+						<img src="zipker-coupon-codes.jpg" />
+					</div>
+					<div class="offerContent">
+						<p>Minimum 30% – 80% Off On Best Selling Products + Free Shipping On All Orders</p>
+
+					</div>
+					<div class="getOffBtn">
+					<button class="getOff"><a href="list.php">Get</a><i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 cradHeight">
+				<div class="offCard">
+					<div class="top">
+						<p>Shop Name</p>
+						<div class="offerPrice"><p class="offLabel">10% off</p></div>
+					</div>
+					<div class="shopLogo">
+						<img src="zipker-coupon-codes.jpg" />
+					</div>
+					<div class="offerContent">
+						<p>Minimum 30% – 80% Off On Best Selling Products + Free Shipping On All Orders</p>
+
+					</div>
+					<div class="getOffBtn">
+					<button class="getOff"><a href="list.php">Get</a><i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 cradHeight">
+				<div class="offCard">
+					<div class="top">
+						<p>Shop Name</p>
+						<div class="offerPrice"><p class="offLabel">10% off</p></div>
+					</div>
+					<div class="shopLogo">
+						<img src="zipker-coupon-codes.jpg" />
+					</div>
+					<div class="offerContent">
+						<p>Minimum 30% – 80% Off On Best Selling Products + Free Shipping On All Orders</p>
+
+					</div>
+					<div class="getOffBtn">
+					<button class="getOff"><a href="list.php">Get</a><i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 cradHeight">
+				<div class="offCard">
+					<div class="top">
+						<p>Shop Name</p>
+						<div class="offerPrice"><p class="offLabel">10% off</p></div>
+					</div>
+					<div class="shopLogo">
+						<img src="zipker-coupon-codes.jpg" />
+					</div>
+					<div class="offerContent">
+						<p>Minimum 30% – 80% Off On Best Selling Products + Free Shipping On All Orders</p>
+
+					</div>
+					<div class="getOffBtn">
+					<button class="getOff"><a href="list.php">Get</a><i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 cradHeight">
+				<div class="offCard">
+					<div class="top">
+						<p>Shop Name</p>
+						<div class="offerPrice"><p class="offLabel">10% off</p></div>
+					</div>
+					<div class="shopLogo">
+						<img src="zipker-coupon-codes.jpg" />
+					</div>
+					<div class="offerContent">
+						<p>Minimum 30% – 80% Off On Best Selling Products + Free Shipping On All Orders</p>
+
+					</div>
+					<div class="getOffBtn">
+					<button class="getOff"><a href="list.php">Get</a><i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+					</div>
+				</div>
+			</div>
+		</div>s
+	</div>s
+	<?php include 'footer/footer.php'; ?>
+		
